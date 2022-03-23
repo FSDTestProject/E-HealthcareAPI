69f503cd61eac5fbbefa7d557a16c6280c460503
AMERICAS\Anjuna_Durgampudi <anjuna_durgampudi@dell.com>
2022-03-22 22:13:37 -0500

    adding order

--- Controllers/CartsController.cs
+++ Controllers/CartsController.cs
@@ -20,14 +20,7 @@ namespace PracticeAPI_Project.Controllers
             _context = context;
         }
 
-        // GET: api/Carts
-        //[HttpGet]
-        //public async Task<ActionResult<IEnumerable<Cart>>> GetCarts()
-        //{
-        //    return await _context.Carts.ToListAsync();
-        //}
-
-        // GET: api/Carts/5
+       
         [HttpGet]
         [Route("GetByUserID")]
         public ActionResult<Cart> GetByUserID(int id)
@@ -40,39 +33,6 @@ namespace PracticeAPI_Project.Controllers
 
             return cart;
         }
-
-        // PUT: api/Carts/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        //[HttpPut("{id}")]
-        //public async Task<IActionResult> PutCart(int id, Cart cart)
-        //{
-        //    if (id != cart.CartId)
-        //    {
-        //        return BadRequest();
-        //    }
-
-        //    _context.Entry(cart).State = EntityState.Modified;
-
-        //    try
-        //    {
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    catch (DbUpdateConcurrencyException)
-        //    {
-        //        if (!CartExists(id))
-        //        {
-        //            return NotFound();
-        //        }
-        //        else
-        //        {
-        //            throw;
-        //        }
-        //    }
-
-        //    return NoContent();
-        //}
-
-
 
         // POST: api/Carts
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
@@ -100,30 +60,43 @@ namespace PracticeAPI_Project.Controllers
             return CreatedAtAction("GetCart", new { id = cartitem.CartId }, cartitem);
         }
 
+               // PUT: api/Orders/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        [Route("PlaceOrder")]
+        public async Task<ActionResult<Order>> PlaceOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException)
+            {
+                if (OrderExists(order.Id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtAction("GetOrder", new { id = order.Id }, order);
+        }
+        private bool CartExists(int id)
+        {
+            return _context.Carts.Any(e => e.CartId == id);
+        }
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.Id == id);
+        }
         private bool CartItemExists(int cartId)
         {
             return _context.CartItems.Any(e => e.CartId == cartId);
         }
 
-        //// DELETE: api/Carts/5
-        //[HttpDelete("{id}")]
-        //public async Task<IActionResult> DeleteCart(int id)
-        //{
-        //    var cart = await _context.Carts.FindAsync(id);
-        //    if (cart == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    _context.Carts.Remove(cart);
-        //    await _context.SaveChangesAsync();
-
-        //    return NoContent();
-        //}
-
-        private bool CartExists(int id)
-        {
-            return _context.Carts.Any(e => e.CartId == id);
-        }
     }
 }
